53cbc3143421302c2e3bf54f95751ae5a822ca99
Azania-Eraste <kouadioazania@gmail.com>
2025-03-19 23:48:02 +0100

    Ajout des pages Shop et shop-detail dans l'application Ecommerce

--- Ecommerce/urls.py
+++ Ecommerce/urls.py
@@ -5,5 +5,8 @@ from Ecommerce import views
 app_name="Ecommerce"
 
 urlpatterns = [
-    path("Dashboard", views.darshbord, name="board"),
+    path("Dashboard/", views.darshbord, name="board"),
+    path("Favorite/", views.favorite, name="favorite"),
+    path("Shop/", views.shop, name="shop"),
+    path("Shop/detail", views.shop_detail, name="detail"),
 ]
