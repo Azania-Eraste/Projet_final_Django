7f37d60c1aea2b28a4617e0d93174d5b7d875d0f
Azania-Eraste <kouadioazania@gmail.com>
2025-04-04 18:20:21 +0200

    Ajout des occurences des categories , produits et variation de produit

--- Ecommerce/urls.py
+++ Ecommerce/urls.py
@@ -6,12 +6,12 @@ app_name="Ecommerce"
 
 urlpatterns = [
     path("Dashboard/", views.dashboard, name="board"),
-    path("Dashboard/delete/<int:id>", views.remove_dahboard, name="board_remove"),
-    path("Dashboard/add/<int:id>", views.add_dashboard, name="board_add"),
+    path("Dashboard/delete/<slug:slug>", views.remove_dahboard, name="board_remove"),
+    path("Dashboard/add/<slug:slug>", views.add_dashboard, name="board_add"),
     path("Dashboard/Chechout", views.checkout, name="check"),
     path("Favorite/", views.favorite, name="favorite"),
-    path("Favorite/add/<int:id>", views.add_favorite, name="favorite_add"),
-    path("Favorite/delete/<int:id>", views.remove_favorite, name="favorite_remove"),
+    path("Favorite/add/<slug:slug>", views.add_favorite, name="favorite_add"),
+    path("Favorite/delete/<slug:slug>", views.remove_favorite, name="favorite_remove"),
     path("Shop/", views.shop, name="shop"),
-    path("Shop/detail/<int:id>", views.shop_detail, name="detail"),
+    path("Shop/detail/<slug:slug>", views.shop_detail, name="detail"),
 ]
