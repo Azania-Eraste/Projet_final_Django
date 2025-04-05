b62b134a8f52f8307e57a4117e0a120fcc06ca65
Azania-Eraste <kouadioazania@gmail.com>
2025-04-05 16:42:45 +0200

    Ajout de Profile,Commande et mode de paiement dans le dashboard

--- Ecommerce/urls.py
+++ Ecommerce/urls.py
@@ -5,13 +5,16 @@ from Ecommerce import views
 app_name="Ecommerce"
 
 urlpatterns = [
-    path("Panier/", views.dashboard, name="panier"),
-    path("Panier/delete/<slug:slug>", views.remove_dahboard, name="panier_remove"),
-    path("Panier/add/<slug:slug>", views.add_dashboard, name="panier_add"),
+    path("Dashboard/Profile/", views.profile_view, name="profile"),
+    path("Dashboard/Commandes/", views.profile_view, name="commandes"),
+    path("Dashboard/Paiement/", views.profile_view, name="paiement"),
+    path("Panier/", views.panier, name="panier"),
+    path("Panier/delete/<slug:slug>", views.remove_panier, name="panier_remove"),
+    path("Panier/add/<slug:slug>", views.add_panier, name="panier_add"),
     path("Panier/Chechout", views.checkout, name="check"),
-    path("Favorite/", views.favorite, name="favorite"),
-    path("Favorite/add/<slug:slug>", views.add_favorite, name="favorite_add"),
-    path("Favorite/delete/<slug:slug>", views.remove_favorite, name="favorite_remove"),
+    path("Dashboard/Favorite/", views.favorite, name="favorite"),
+    path("Dashboard/Favorite/add/<slug:slug>", views.add_favorite, name="favorite_add"),
+    path("Dashboard/Favorite/delete/<slug:slug>", views.remove_favorite, name="favorite_remove"),
     path("Shop/", views.shop, name="shop"),
     path("Shop/detail/<slug:slug>", views.shop_detail, name="detail"),
 ]
